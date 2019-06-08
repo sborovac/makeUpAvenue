2102119753ce7e05855fbc6927579965ed9a600f
catlover98 <sborovac1@etf.unsa.ba>
2019-06-08 20:04:27 +0200

    backend for users added

---
+++ BACKEND/makeupAvenue/makeupAvenue/Models/User.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace makeupAvenue.Models
+{
+    public class User
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("name")]
+        public string Name { get; set; }
+
+        [BsonElement("dob")]
+        public string dob { get; set; }
+
+        [BsonElement("email")]
+        public string email { get; set; }
+
+        [BsonElement("username")]
+        public string username { get; set; }
+
+        [BsonElement("password")]
+        public string password { get; set; }
+
+        [BsonElement("creditcard")]
+        public string creditCard { get; set; }
+    }
+}
